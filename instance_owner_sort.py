71aa7f1193fd406734443cf4816d3662433332d2
Matt Misner <mwmisner@gmail.com>
2016-11-28 03:50:39 +0000

    Add sort functionaltiy

--- instance_owner_sort.py
+++ instance_owner_sort.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 # Reimplementation of ruby version, not fully working
 import boto3
+from operator import itemgetter, attrgetter, methodcaller
 
 ec2 = boto3.client('ec2')
 
 reservations = ec2.describe_instances()
-instance_data = [['InstanceId','PrivateIp Address',]]
-
-max_length = [0] * (len(instance_data) + 1)
+instance_data_headers = ['InstanceId','PrivateIpAddress']
+sort_index = instance_data_headers.index('InstanceId')
+instance_data = []
+max_length = [0] * (len(instance_data_headers) + 1)
 
 for instances in reservations['Reservations']:
     instance = instances['Instances'][0]
     instance_data.append([instance['InstanceId'],instance['PrivateIpAddress']])
+    print instance , "\n"
+
+instance_data.insert(0, instance_data_headers )
 
 for row in instance_data:
     x = 0
@@ -20,17 +25,17 @@ for row in instance_data:
         if length > max_length[x]:
             max_length[x] = length
         x += 1
+instance_data.pop(0)
+sorted_instance_data = sorted(instance_data, key=itemgetter(sort_index))
+sorted_instance_data.insert(0, instance_data_headers )
 
-for row in instance_data:
+for row in sorted_instance_data:
     x=0
-    formatted_row = [' '] * (len(instance_data) + 1)
+    formatted_row = [' '] * len(row)
     for item in row:
-        print x
         padding = ''
-        if (max_length[x] - len(row)) > 0:
-            padding = ' ' * (max_length[x] - len(row[x]))
+        if (max_length[x] - len(item)) > 0:
+            padding = ' ' * (max_length[x] - len(item[x]))
         formatted_row[x] = item + padding
         x += 1
-        print item
-        
     print ' | '.join(formatted_row)
